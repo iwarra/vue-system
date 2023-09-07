849de9fec7522fd8d3a4d2a2669b3460d4e375f7
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-09-07 10:35:54 +0200

    Added icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from "./router";
 import { createPinia } from "pinia";
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(mdiVue, {
+    icons: mdijs
+  })
+  .mount('#app')
