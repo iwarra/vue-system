fc2e737c2dc40169c87e55fa09ec8e3c99d9fe71
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-09-30 13:32:39 +0200

    minor update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 import router from "./router";
 import { createPinia } from "pinia";
-import mdiVue from 'mdi-vue/v3'
-import * as mdijs from '@mdi/js'
+import mdiVue from 'mdi-vue/v3';
+import * as mdijs from '@mdi/js';
 
 const pinia = createPinia();
 
