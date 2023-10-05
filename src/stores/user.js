985bee010b8a501f5ab0d38351f65de1d54050e0
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-10-05 16:12:34 +0200

    Added a localStorage check for username

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,9 +1,12 @@
 import { defineStore } from "pinia";
 
+let userCheck = localStorage.getItem('user');
+let storedUser = JSON.parse(userCheck);
+
 export const useUserStore = defineStore({
   id: "user",
   state: () => ({
-    username: "",
+    username: userCheck ? storedUser.username : '',
     isLoggedIn: false
   }),
   actions: {
