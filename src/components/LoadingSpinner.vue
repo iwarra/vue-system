69640a9cd53399912a9204337511aa8c9072a0e8
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-10-06 09:06:43 +0200

    Changed spinner color

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -19,7 +19,7 @@
   top: 73.79px;
   position: absolute;
   animation: ldio-37t7hale7vr linear 1s infinite;
-  background: #3e6d8d;
+  background: var(--primary-accent);
   width: 18.84px;
   height: 40.82px;
   border-radius: 9.42px / 20.41px;
@@ -27,51 +27,51 @@
 }.ldio-37t7hale7vr div:nth-child(1) {
   transform: rotate(0deg);
   animation-delay: -0.9166666666666666s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(2) {
   transform: rotate(30deg);
   animation-delay: -0.8333333333333334s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(3) {
   transform: rotate(60deg);
   animation-delay: -0.75s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(4) {
   transform: rotate(90deg);
   animation-delay: -0.6666666666666666s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(5) {
   transform: rotate(120deg);
   animation-delay: -0.5833333333333334s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(6) {
   transform: rotate(150deg);
   animation-delay: -0.5s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(7) {
   transform: rotate(180deg);
   animation-delay: -0.4166666666666667s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(8) {
   transform: rotate(210deg);
   animation-delay: -0.3333333333333333s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(9) {
   transform: rotate(240deg);
   animation-delay: -0.25s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(10) {
   transform: rotate(270deg);
   animation-delay: -0.16666666666666666s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(11) {
   transform: rotate(300deg);
   animation-delay: -0.08333333333333333s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }.ldio-37t7hale7vr div:nth-child(12) {
   transform: rotate(330deg);
   animation-delay: 0s;
-  background: #3e6d8d;
+  background: var(--primary-accent);
 }
 .loadingio-spinner-spinner-c1lc0tehm7v {
   width: 314px;
